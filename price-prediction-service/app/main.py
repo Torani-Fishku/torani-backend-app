00dd8e245520ffe8ed3cc5d9cf3d763c8ed3574e
eduardoimanuel <110005459+eduardoimanuel@users.noreply.github.com>
2023-06-08 12:32:32 +0700

    Update main.py
    - debugged the functions
    - add generate_predictions function

--- price-prediction-service/app/main.py
+++ price-prediction-service/app/main.py
@@ -36,10 +36,10 @@ load_all_models()
 
 # Load all dataset and preprocess
 window_size=8
-#loaded_datasets = {}
 normalized_datasets = {}
+date_data = {}
 windowed_data= {}
-min_max_data={}
+min_max_data= {}
 
 def min_max(data,fish_type):
     # Store the min and max price for unscaling later
@@ -51,7 +51,7 @@ def normal_scaler(data,fish_type):
     # Compute the minimum and maximum values
     data_min, data_max = min_max_data[fish_type]['Min'],min_max_data[fish_type]['Max']
     # Scale the data using the min-max formula
-    scaled_data = [(x - data_min) / (data_max - data_min) for x in data]
+    scaled_data = (data - data_min) / (data_max - data_min)
     return scaled_data
 
 def unscaler(data,fish_type):
@@ -60,29 +60,63 @@ def unscaler(data,fish_type):
     unscaled_data = (data*(data_max-data_min))+data_min
     return unscaled_data
 
+def fish_date(data,fish_type):
+    date=pd.to_datetime(data)
+    date_data[fish_type]=date
+
 def preprocess_data(data_paths,window_size):
+    # Load and normalize the fish price data
     for fish_type, data_path in data_paths.items():
-        df = pd.read_csv(data_path).tail(window_size)
-        #loaded_datasets[fish_type] = df
+        df = pd.read_csv(data_path)
         min_max(df['harga'],fish_type)
+        fish_date(df['tanggal_input'],fish_type)
         normalized_datasets[fish_type] = normal_scaler(df['harga'],fish_type)
 
 def df_to_X(df, window):
-  df_as_np = df.to_numpy()
+  # Make the tail of the price data to array
+  # The array size is in the form of input of LSTM Neural Network
+  df_as_np = df.tail(window).to_numpy()
   X = []
-  #y = []
-  for i in range(len(df_as_np)-window):
+  for i in range(len(df_as_np)-window+1):
     row = [[a] for a in df_as_np[i:i+window]]
     X.append(row)
-    #label = df_as_np[i+window]
-    #y.append(label)
-  return np.array(X, dtype=float) #, np.array(y, dtype=float)
+  return np.array(X, dtype=float)
 
-def windowing_data(dataframes):
-    for fish_type, dataframe in dataframes.items():
-        windowed_X = df_to_X(dataframe['harga'],window_size)
+def windowing_data(dictionary):
+    #Loop for windowing the data by executing df_to_X function for each fish
+    for fish_type, item in dictionary.items():
+        windowed_X = df_to_X(item,window_size)
         windowed_data[fish_type]=windowed_X
+        
+def generate_predictions(n_time, fish_type, window=window_size):
+    # Create an empty dataframe to store the predictions
+    predictions_df = pd.DataFrame(columns=['Date', 'Price'])
+    
+    # Load the fish prediction model  
+    model = loaded_models.get(fish_type)
+    
+    # Load the fish price date
+    start_date= date_data.get(fish_type).iloc[-1]
 
+    # Reshape the input_data to maintain the original shape for future predictions
+    input_data = windowed_data[fish_type].reshape(1, -1, 1)
+    
+    # Generate predictions for each day ahead
+    for i in range(n_time):
+        # Prepare the input data for prediction
+        input_data_pred = input_data[:, int(-1*window):, :]  # Use the last 8 values for prediction
+        
+        # Predict the next price using the model
+        predicted_price = model.predict(input_data_pred)
+        # Add the prediction to the dataframe
+        prediction_row = {'Date': start_date + pd.DateOffset(days=i), 'Price': unscaler(predicted_price,fish_type)[0][0]}
+        predictions_df = predictions_df.append(prediction_row, ignore_index=True)
+        
+        # Update the input data for the next prediction
+        input_data = np.concatenate((input_data, predicted_price.reshape(1, 1, 1)), axis=1)
+
+    return predictions_df
+        
 preprocess_data(data_paths=data_paths,window_size=window_size)
 windowing_data(normalized_datasets)
     
@@ -93,22 +127,15 @@ async def root():
 
 # GET /predict/{fish_type}/{date}
 @app.get("/predict/{fish_type}/{date}")
-async def predict_price(start_date: str,  fish_type: str, input_data=windowed_data[fish_type],days=7):
-    # Preprocess the input date if necessary
-    # Perform any required data manipulation or transformation
-    predictions_df = pd.DataFrame(columns=['Date', 'Price'])
-    # Make the prediction using the loaded model
+async def predict_price(date: str,  fish_type: str):
+
+    # Load the model
     model = loaded_models.get(fish_type)
     if model is None:
         return {"error": "Invalid fish type, currently there's no such fish in the model"}
 
-    input_data[fish_type] = input_data[fish_type].to_numpy().reshape(1, -1, 1)
-    for i in range(days):
-        prediction = model.predict(input_data[fish_type])
-        pred_row={'Date': start_date + pd.DateOffset(days=i), 'Price': unscaler(prediction,fish_type)[0][0]}
-        predictions_df=predictions_df.append(pred_row, ignore_index=True)
-        # Update the input data for the next prediction
-        input_data = np.concatenate((input_data, prediction.reshape(1, 1, 1)), axis=1)
-
+    predictions_df=generate_predictions(n_time=7, fish_type=fish_type)
+    #date_predicted=predictions_df[predictions_df['Date']=='2023-02-10']['Date'] #.dt.strftime('%Y-%m-%d').values[0]
+    price_predicted=float(predictions_df[predictions_df['Date']==date]['Price'])
     # Return the prediction as a response
-    return predictions_df
+    return {'date': date, 'predicted_price': price_predicted}
